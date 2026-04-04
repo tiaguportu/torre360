8e1d0232aa24d660de870aa8c47f32769ff54ac3
portotpb <portotpb@fab.mil.br>
2026-04-04 19:43:44 -0300

    Implementado criação em lote de Títulos no TituloResource

--- app/Filament/Resources/Pessoas/PessoaResource.php
+++ app/Filament/Resources/Pessoas/PessoaResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Resources\Pessoas;
 
-use App\Filament\Resources\Concerns\HasNavigationBadge;
 use App\Filament\Resources\Pessoas\Pages\CreatePessoa;
 use App\Filament\Resources\Pessoas\Pages\EditPessoa;
 use App\Filament\Resources\Pessoas\Pages\ListPessoas;
@@ -19,8 +18,6 @@ use Illuminate\Database\Eloquent\Builder;
 
 class PessoaResource extends Resource
 {
-    use HasNavigationBadge;
-
     protected static ?string $model = Pessoa::class;
 
     protected static string|\UnitEnum|null $navigationGroup = 'Cadastros';
