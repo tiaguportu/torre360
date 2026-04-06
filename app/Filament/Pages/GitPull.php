570153e69f70cdbaacac8bd0d4fec925d1efd024
portotpb <portotpb@fab.mil.br>
2026-04-06 07:53:30 -0300

    algo

---
+++ app/Filament/Pages/GitPull.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Notifications\Notification;
+use Filament\Pages\Page;
+use Illuminate\Support\Facades\Process;
+
+class GitPull extends Page
+{
+    protected static \BackedEnum|string|null $navigationIcon = 'heroicon-o-arrow-path';
+
+    protected string $view = 'filament.pages.git-pull';
+
+    protected static ?string $title = 'Atualizar Sistema (Git Pull)';
+
+    protected static ?string $navigationLabel = 'Git Pull';
+
+    protected static ?string $slug = 'git-pull';
+
+    protected static bool $shouldRegisterNavigation = false;
+
+    public function runGitPull(): void
+    {
+        if (! auth()->user()->hasRole('super_admin')) {
+            Notification::make()
+                ->title('Acesso Negado')
+                ->danger()
+                ->send();
+
+            return;
+        }
+
+        $result = Process::run('git pull origin main');
+
+        if ($result->successful()) {
+            Notification::make()
+                ->title('Sistema Atualizado')
+                ->body($result->output())
+                ->success()
+                ->send();
+        } else {
+            Notification::make()
+                ->title('Erro na Atualização')
+                ->body($result->errorOutput())
+                ->danger()
+                ->persistent()
+                ->send();
+        }
+    }
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()?->hasRole('super_admin') ?? false;
+    }
+}
