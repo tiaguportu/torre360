5dad78c64f831003405b3471d9ef991bf2eb10fc
portotpb <portotpb@fab.mil.br>
2026-04-06 06:26:09 -0300

    Ajustes

--- resources/views/filament/resources/matriculas/columns/dropzone-documento.blade.php
+++ resources/views/filament/resources/matriculas/columns/dropzone-documento.blade.php
@@ -1,15 +1,9 @@
-<div
-    x-data="{
+<div x-data="{
         isUploading: false,
         isDropping: false,
         progress: 0,
-    }"
-    class="relative group"
->
-    <div
-        x-on:dragover.prevent="isDropping = true"
-        x-on:dragleave.prevent="isDropping = false"
-        x-on:drop.prevent="
+    }" class="relative group">
+    <div x-on:dragover.prevent="isDropping = true" x-on:dragleave.prevent="isDropping = false" x-on:drop.prevent="
             isDropping = false;
             if ($event.dataTransfer.files.length > 0) {
                 const file = $event.dataTransfer.files[0];
@@ -38,21 +32,17 @@
                     progress = event.detail.progress;
                 });
             }
-        "
-        :class="{
+        " :class="{
             'border-warning-500 bg-warning-50 dark:bg-warning-900/10': isDropping,
             'border-gray-300 dark:border-gray-700': !isDropping
         }"
         class="flex flex-col items-center justify-center p-2 transition-all border-2 border-dashed rounded-lg cursor-pointer hover:border-warning-500 hover:bg-warning-50 dark:hover:bg-warning-900/10"
-        title="Arraste um arquivo PDF ou Imagem para enviar"
-    >
+        title="Arraste um arquivo PDF ou Imagem para enviar">
         <template x-if="!isUploading">
             <div class="flex items-center gap-2 text-sm font-medium text-gray-500 dark:text-gray-400">
-                <x-filament::icon
-                    icon="heroicon-o-cloud-arrow-up"
-                    class="w-5 h-5 text-gray-400 group-hover:text-warning-500 transition-colors"
-                />
-                <span class="group-hover:text-warning-600 transition-colors">Arraste aqui</span>
+                <x-filament::icon icon="heroicon-o-cloud-arrow-up"
+                    class="w-5 h-5 text-gray-400 group-hover:text-warning-500 transition-colors" />
+                <span class="group-hover:text-warning-600 transition-colors">!Arraste aqui</span>
             </div>
         </template>
 
@@ -63,10 +53,8 @@
                     <span class="text-[10px] font-medium text-warning-600 font-mono" x-text="progress + '%'"></span>
                 </div>
                 <div class="w-full h-1.5 bg-gray-200 rounded-full dark:bg-gray-700 overflow-hidden">
-                    <div
-                        class="h-full transition-all duration-300 bg-warning-600 rounded-full shadow-[0_0_8px_rgba(var(--warning-600),0.5)]"
-                        :style="'width: ' + progress + '%'"
-                    ></div>
+                    <div class="h-full transition-all duration-300 bg-warning-600 rounded-full shadow-[0_0_8px_rgba(var(--warning-600),0.5)]"
+                        :style="'width: ' + progress + '%'"></div>
                 </div>
             </div>
         </template>
