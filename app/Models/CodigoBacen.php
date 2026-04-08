d284101b9a1595f85826d7ed2423d979a7738896
portotpb <portotpb@fab.mil.br>
2026-04-08 17:20:01 -0300

    fix: adicionando protecao de mass assignment ao modelo CodigoBacen

--- app/Models/CodigoBacen.php
+++ app/Models/CodigoBacen.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CodigoBacen extends Model
 {
-    //
+    protected $table = 'codigo_bacens';
+
+    protected $guarded = [];
+
+    public function bancos(): HasMany
+    {
+        return $this->hasMany(Banco::class);
+    }
 }
