2fa4f9565158678542d6664db24813e52f3e30cb
portotpb <portotpb@fab.mil.br>
2026-04-18 09:38:35 -0300

    refactor: centraliza formulário de endereço no RelationManager de Pessoas para garantir consistência visual e ViaCEP

--- app/Filament/Resources/Pessoas/RelationManagers/EnderecoRelationManager.php
+++ app/Filament/Resources/Pessoas/RelationManagers/EnderecoRelationManager.php
@@ -2,14 +2,12 @@
 
 namespace App\Filament\Resources\Pessoas\RelationManagers;
 
-use App\Filament\Resources\Cidades\Schemas\CidadeForm;
+use App\Filament\Resources\Enderecos\Schemas\EnderecoForm;
 use Filament\Actions\AttachAction;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DetachAction;
 use Filament\Actions\EditAction;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
@@ -23,33 +21,7 @@ class EnderecoRelationManager extends RelationManager
 
     public function form(Schema $schema): Schema
     {
-        return $schema
-            ->components([
-                Select::make('tipo')
-                    ->options([
-                        'residencial' => 'Residencial',
-                        'comercial' => 'Comercial',
-                    ])
-                    ->default('residencial')
-                    ->required(),
-                Select::make('cidade_id')
-                    ->relationship('cidade', 'nome', fn ($query) => $query->whereNotNull('nome'))
-                    ->searchable()
-                    ->preload()
-                    ->createOptionForm(fn (Schema $schema) => CidadeForm::configure($schema)->getComponents())
-                    ->required(),
-                TextInput::make('logradouro')
-                    ->required()
-                    ->maxLength(255),
-                TextInput::make('numero')
-                    ->maxLength(10),
-                TextInput::make('complemento')
-                    ->maxLength(255),
-                TextInput::make('bairro')
-                    ->maxLength(255),
-                TextInput::make('cep')
-                    ->maxLength(10),
-            ]);
+        return EnderecoForm::configure($schema);
     }
 
     public function table(Table $table): Table
