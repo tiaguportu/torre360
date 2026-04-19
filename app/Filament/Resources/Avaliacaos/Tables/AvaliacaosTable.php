e691304b359fad41bcc6885bea5ee83632d991e0
portotpb <portotpb@fab.mil.br>
2026-04-19 18:53:57 -0300

    fix: corrigir namespace da Action na listagem de avaliacoes para V5

--- app/Filament/Resources/Avaliacaos/Tables/AvaliacaosTable.php
+++ app/Filament/Resources/Avaliacaos/Tables/AvaliacaosTable.php
@@ -8,7 +8,7 @@ use App\Notifications\SystemNotification;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Tables\Actions\Action;
+use Filament\Actions\Action;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
