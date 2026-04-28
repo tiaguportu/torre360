269c537bee87c5126ec754349738624cdf109bd1
Tiago Porto <tiaguportu@gmail.com>
2026-04-28 06:39:34 -0300

    fix(preceptoria): permite quebra de linha com HTML na descrição do widget de agendamento

--- app/Filament/Widgets/PreceptoriaSchedulingWidget.php
+++ app/Filament/Widgets/PreceptoriaSchedulingWidget.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\HtmlString;
 
 class PreceptoriaSchedulingWidget extends BaseWidget
 {
@@ -22,7 +23,7 @@ class PreceptoriaSchedulingWidget extends BaseWidget
         /** @var User $user */
         $user = Auth::user();
 
-        if (!$user) {
+        if (! $user) {
             return [];
         }
 
@@ -64,7 +65,7 @@ class PreceptoriaSchedulingWidget extends BaseWidget
                 $prof = current(explode(' ', $agendamentoFuturo->professor?->nome ?? 'S/P'));
 
                 $stats[] = Stat::make($matricula->pessoa->nome, 'Preceptoria Agendada')
-                    ->description("{$data} às {$hora}<br>Prof. {$prof}")
+                    ->description(new HtmlString("{$data} às {$hora}<br>Prof. {$prof}"))
                     ->descriptionIcon('heroicon-m-check-circle')
                     ->color('success');
             } else {
@@ -86,7 +87,7 @@ class PreceptoriaSchedulingWidget extends BaseWidget
                         ->color('warning')
                         ->extraAttributes([
                             'class' => 'cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 transition',
-                            'onclick' => "window.location.href='" . route('filament.admin.resources.preceptorias.agendar') . "?matricula_id={$matricula->id}'",
+                            'onclick' => "window.location.href='".route('filament.admin.resources.preceptorias.agendar')."?matricula_id={$matricula->id}'",
                         ]);
                 }
             }
