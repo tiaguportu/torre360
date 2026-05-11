329c52b7f1df5849d5e9974d33b20e011c1032f5
Tiago Porto <tiaguportu@gmail.com>
2026-05-10 22:18:52 -0300

    fix: restaura namespaces corretos das acoes (Filament\Actions)

--- app/Filament/Resources/Questionarios/Tables/QuestionariosTable.php
+++ app/Filament/Resources/Questionarios/Tables/QuestionariosTable.php
@@ -4,11 +4,11 @@ namespace App\Filament\Resources\Questionarios\Tables;
 
 use App\Filament\Resources\Questionarios\QuestionarioResource;
 use App\Models\Questionario;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ViewAction;
+use Filament\Actions\Action;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
