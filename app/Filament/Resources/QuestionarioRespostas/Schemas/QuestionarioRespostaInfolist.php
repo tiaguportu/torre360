e8e23b36a1b81ea69c6bdefead409fd4b849d430
Tiago Porto <tiaguportu@gmail.com>
2026-05-13 21:48:51 -0300

    style: empilha secoes principais na visualizacao de respostas

--- app/Filament/Resources/QuestionarioRespostas/Schemas/QuestionarioRespostaInfolist.php
+++ app/Filament/Resources/QuestionarioRespostas/Schemas/QuestionarioRespostaInfolist.php
@@ -93,6 +93,7 @@ class QuestionarioRespostaInfolist
                                     ->compact(),
                             ]),
                     ]),
-            ]);
+            ])
+            ->columns(1);
     }
 }
