1b528645b82a7ed45216f3a8605b0064555a0ec3
Tiago Porto <tiaguportu@gmail.com>
2026-05-13 22:17:46 -0300

    style(questionarios): garante quebra de linha em tags p e outros elementos internos

--- resources/views/filament/resources/questionario-respostas/comparacao.blade.php
+++ resources/views/filament/resources/questionario-respostas/comparacao.blade.php
@@ -85,7 +85,7 @@
                         @foreach ($perguntasRow as $key => $valores)
                             <tr class="fi-ta-row transition duration-75 hover:bg-gray-50 dark:hover:bg-white/5">
                                 <td class="fi-ta-cell px-3 py-4 sm:first-of-type:ps-6 sm:last-of-type:pe-6 align-top bg-gray-50/50 dark:bg-white/5 w-[300px] min-w-[250px] max-w-[350px]">
-                                    <div class="prose dark:prose-invert max-w-full break-words text-sm">{!! $perguntasLabels[$key] !!}</div>
+                                    <div class="prose dark:prose-invert max-w-full break-words text-sm [&_*]:break-words [&_*]:whitespace-normal">{!! $perguntasLabels[$key] !!}</div>
                                     @if(!str_starts_with($key, 'pergunta_'))
                                         <div class="mt-1 text-xs text-gray-500 dark:text-gray-400 font-mono">ID: {{ $key }}</div>
                                     @endif
@@ -96,7 +96,7 @@
                                             @if(empty($valores[$resposta->id]))
                                                 (Vazio)
                                             @else
-                                                <div class="prose dark:prose-invert max-w-full break-words text-sm text-gray-600 dark:text-gray-400">{!! $valores[$resposta->id] !!}</div>
+                                                <div class="prose dark:prose-invert max-w-full break-words text-sm text-gray-600 dark:text-gray-400 [&_*]:break-words [&_*]:whitespace-normal">{!! $valores[$resposta->id] !!}</div>
                                             @endif
                                         @else
                                             <span class="text-gray-300 dark:text-gray-600">-</span>
