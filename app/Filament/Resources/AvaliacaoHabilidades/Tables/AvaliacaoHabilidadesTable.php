4b04db96a6cf0dca449394a08afa188cec4021a5
Tiago Porto <tiaguportu@gmail.com>
2026-06-05 16:02:07 -0300

    Corrige importações de Ações no Filament (usa Filament\Actions) e define columnSpanFull para o formulário

--- app/Filament/Resources/AvaliacaoHabilidades/Tables/AvaliacaoHabilidadesTable.php
+++ app/Filament/Resources/AvaliacaoHabilidades/Tables/AvaliacaoHabilidadesTable.php
@@ -3,10 +3,10 @@
 namespace App\Filament\Resources\AvaliacaoHabilidades\Tables;
 
 use App\Filament\Resources\AvaliacaoHabilidades\AvaliacaoHabilidadeResource;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
+use Filament\Actions\Action;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
