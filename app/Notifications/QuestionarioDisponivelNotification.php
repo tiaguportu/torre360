5878b6505e490f847ddb39f7303ef4e2e60251ec
Tiago Porto <tiaguportu@gmail.com>
2026-06-18 07:58:14 -0300

    feat: adicionar canais de push e database (sininho) na notificação de questionários

--- app/Notifications/QuestionarioDisponivelNotification.php
+++ app/Notifications/QuestionarioDisponivelNotification.php
@@ -4,6 +4,9 @@ namespace App\Notifications;
 
 use App\Filament\Resources\Questionarios\QuestionarioResource;
 use App\Models\Questionario;
+use App\Notifications\Channels\FcmChannel;
+use Filament\Actions\Action as FilamentAction;
+use Filament\Notifications\Notification as FilamentNotification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -17,7 +20,7 @@ class QuestionarioDisponivelNotification extends Notification implements ShouldQ
 
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['mail', 'database', FcmChannel::class];
     }
 
     public function toMail(object $notifiable): MailMessage
@@ -31,5 +34,52 @@ class QuestionarioDisponivelNotification extends Notification implements ShouldQ
             ->line('Sua opinião é muito importante para nós. Por favor, dedique alguns minutos para respondê-lo.')
             ->action('Responder Questionário', $url)
             ->line('Se você já respondeu ou se este aviso não for aplicável, por favor desconsidere.');
+    }
+
+    public function toDatabase(object $notifiable): array
+    {
+        $url = QuestionarioResource::getUrl('responder', ['record' => $this->questionario]);
+
+        $notification = FilamentNotification::make()
+            ->title('Questionário Disponível')
+            ->body('O questionário "'.$this->questionario->titulo.'" está disponível para resposta.')
+            ->info();
+
+        if ($url) {
+            $notification->actions([
+                FilamentAction::make('view')
+                    ->label('Responder')
+                    ->url($url)
+                    ->button(),
+            ]);
+        }
+
+        return $notification->getDatabaseMessage();
+    }
+
+    public function toPush(object $notifiable): array
+    {
+        $url = QuestionarioResource::getUrl('responder', ['record' => $this->questionario]);
+
+        return [
+            'title' => 'Questionário Disponível',
+            'body' => 'O questionário "'.$this->questionario->titulo.'" está disponível para resposta.',
+            'data' => [
+                'url' => $url,
+                'type' => 'questionario',
+            ],
+        ];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        $url = QuestionarioResource::getUrl('responder', ['record' => $this->questionario]);
+
+        return [
+            'title' => 'Questionário Disponível',
+            'body' => 'O questionário "'.$this->questionario->titulo.'" está disponível para resposta.',
+            'action_url' => $url,
+            'type' => 'questionario',
+        ];
     }
 }
