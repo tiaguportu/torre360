d85d3de74b450926b594770755578323d5380d4f
Tiago Porto <tiaguportu@gmail.com>
2026-07-02 21:08:38 -0300

    fix: Corrige namespaces das actions no TemplateCrachaV2STable para o Filament V5

--- app/Filament/Resources/TemplateCrachaV2S/Tables/TemplateCrachaV2STable.php
+++ app/Filament/Resources/TemplateCrachaV2S/Tables/TemplateCrachaV2STable.php
@@ -3,10 +3,10 @@
 namespace App\Filament\Resources\TemplateCrachaV2S\Tables;
 
 use App\Models\TemplateCrachaV2;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
+use Filament\Actions\Action;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
